220141b7d300e7617119f88d6bc96a78916ef65f
gahara <madinikus@gmail.com>
2021-08-07 23:27:39 +0300

    added main functionality

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# picker_bot
+# Picker
 picks random user from chat
+
+
+### main command: ```/who```
+
+Also responds to ```/кто? /кто /who?```
+
+Should be added to chat without admin's privileges, 
+so it can only access messages starting with ```'/'```
+
+Writen in Yandex.Cloud functions style <br>
+See  more information at https://cloud.yandex.ru/docs/functions/
---
+++ index.py
@@ -0,0 +1,69 @@
+from pyrogram import Client
+import random
+import os
+import json
+
+token = os.environ['BOT_TOKEN']
+api_id = os.environ['API_ID']
+api_hash = os.environ['API_HASH']
+bot_username = os.environ['BOT_USER_NAME']
+
+app = Client(session_name=":memory:", bot_token=token, api_id=api_id, api_hash=api_hash)
+
+filters = (["/кто?", "/кто", "/who?", "/who"])
+
+
+def echo(client, message):
+    chat = message.get('chat')
+    try:
+        app.start()
+    except:
+        print('Already started')
+    
+    users = app.get_chat_members(chat.get('id'))
+    app.stop()
+    formatted_users = []
+    for user in users:
+        if not user.user.is_bot:
+            formatted_users.append(user.user.username if user.user.username else "[{tuname}](tg://user?id={uid})".format(
+                uid=user.user.id,
+                tuname=user.user.first_name))
+    return "@{username}".format(username=random.choice(formatted_users))
+
+
+def runner(event, context):
+    body = json.loads(event.get('body'))
+    message = body.get('message')
+    chat = message.get('chat')
+
+    if message.get('text') in filters:
+        answer = {
+            "method": "sendMessage",
+            "chat_id": chat.get('id'),
+            "reply_to_message_id": message.get('message_id'),
+            "text": echo('', message)
+        }
+
+        res = {
+            "statusCode": 200,
+            "headers": {
+                'Content-Type': 'application/json'
+            },
+            "body": json.dumps(answer)
+        }
+        return res
+    else:
+        answer = {
+            "method": "sendMessage",
+            "chat_id": chat.get('id'),
+            "reply_to_message_id": message.get('message_id'),
+            "text": "Unknown command"
+        }
+        res = {
+            "statusCode": 200,
+            "headers": {
+                'Content-Type': 'application/json'
+            },
+            "body": json.dumps(answer)
+        }
+        return res
